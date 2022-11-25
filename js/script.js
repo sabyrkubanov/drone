5d52d3cc0b0b59a10033e368a9c958a7315a0934
sabyrkubanov <kubanovski.01@gmail.com>
2022-11-25 11:45:18 +0600

    add drone

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("input")
+const body = document.querySelector("body")
+
+const toggleThemeMode =( )=>{
+    body.classList.toggle("dark")
+};
+input.onchange = toggleThemeMode;
